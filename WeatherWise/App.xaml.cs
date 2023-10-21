6afe0f7223404478618c3dd7057ad25c6abac780
majidpawar9 <majidpawar9@gmail.com>
2023-10-21 00:22:26 -0400

    init

---
+++ WeatherWise/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherWise;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WeatherWise/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherWise;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
